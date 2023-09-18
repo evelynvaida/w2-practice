dedf27e33b42a2bfb701c0b5ca4d494be6b6518d
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-09-18 21:45:02 +0300

    js functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function logger(text) {
+    console.log(text)
+}
+
+/* logger('hello')
+logger('szia') */
+
+function logTheSumTwoNumbers(a, b) {
+    console.log(a + b);
+}
+
+/* logTheSumTwoNumbers(5, 10) */
+
+function sumTwoNumbers(a, b) {
+    let sum = 0;
+    sum = a + b;
+
+    return sum;
+}
+
+logger (sumTwoNumbers(5, 10))
