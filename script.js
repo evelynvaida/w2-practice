8b16ec83a019bccc59a6462cbbad0f419f58d3a8
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-09-20 12:34:07 +0300

    homework, new function

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-function logger(text) {
+/* function logger(text) {
     console.log(text)
-}
+} */
 
 /* logger('hello')
 logger('szia') */
 
-function logTheSumTwoNumbers(a, b) {
+/* function logTheSumTwoNumbers(a, b) {
     console.log(a + b);
-}
+} */
 
 /* logTheSumTwoNumbers(5, 10) */
 
-function sumTwoNumbers(a, b) {
+/* function sumTwoNumbers(a, b) {
     let sum = 0;
     sum = a + b;
 
     return sum;
 }
 
-logger (sumTwoNumbers(5, 10))
+logger (sumTwoNumbers(5, 10)) */
+
+function calculateAge(birthYear, currentYear) {
+    var age = (currentYear - birthYear)-1;
+
+    return age
+}
+
+function yourAge (text) {
+    console.log("You are " + text + " years old")
+}
+
+yourAge (calculateAge(1993,2023))
