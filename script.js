17ebbbf90b6c43c6027e1013a2955da9adf96ce6
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-09-22 19:38:46 +0300

    for loop

--- script.js
+++ script.js
@@ -1,93 +1,29 @@
-/* let stringVariable = "text";
-let numberVariable = 15;
-let booleanVariable = true;
+console.log("loaded");
 
-function myFunc(param1, param2) {
-    return param1 + param2
-}
+let rootElement = document.querySelector("#root")
+//console.log(rootElement)
 
-console.log(stringVariable);
-console.log(numberVariable, stringVariable); */
+//let htmlContent = "<h2>Hello world</h2>"
 
-/* let array = ["alma", 12, false, undefined, null]
+//rootElement.insertAdjacentHTML("beforeend",htmlContent)
 
-console.log(array)
+/* let counter = 0;
 
-let stringArray = ["alma", "korte", "barack"];
+while (counter < 10) {
+    rootElement.insertAdjacentHTML("beforeend",htmlContent);
+    counter++;
+} */
 
-console.log(stringArray[4]); */
-/* 
-let myObj = {
-    key1: "value1",
-    key2: "value2",
-    Key4: 15,
-    key6: true,
-    key7: ["alma", "korte", "barack"]
-}
+/* for (let counter = 0; counter <= 10; counter++) {
+    rootElement.insertAdjacentHTML("beforeend",htmlContent);
+} */
 
-console.log(myObj.key7[1]); */
+//let array = ["alma", 'korte', 'szilva'];
 
-/* let myFacebook = {
-    name: "Evelyn Vaida",
-    age: 28,
-    location: "Kolozsvar",
-    friends: [
-        "pelda peter",
-        "pelda peter2"
-    ],
-    movies: [
-        {
-            title: "Harry Potter",
-            year: 2001,
-            casts: [
-                "Harry Potter",
-                "Hadvig"
-            ]
-        }, {
-            title: "Ananasz Express",
-            year: 2010,
-        }
-    ]
-}
+/* for (let counter = 0; counter <= 10; counter++) {
+    rootElement.insertAdjacentHTML("beforeend",`<h2> ${array[0]} </h2>`);
+} */
 
-console.log(myFacebook.movies[0].casts[1])
-
-let myArr = [
-    ["alma", "korte"],
-    ["szentkiralyi", "tesco gazd"]
-]
-
-console.log(myArr[1][0]) */
-
-function negativeOrPositive(number) {
-    if (number < 0) {
-        return "negative"
-    } else if (number > 0) {
-        return "positive"
-    } else {
-        return "zero"
-    }
-}
-
-function logger(text) {
-    console.log(text);
-}
-
-// logger(negativeOrPositive(0))
-
-function decideIfBeerIsGood(beer) {
-    /* beer = {name: "Dreher", price: 349, abv: 5} */      /* &&=AND , ||=OR */
-    if(beer.price < 340 && beer.abv > 4.7) {
-        return true
-    } else {
-        return false
-    }
-}
-
-let dreher = {name: "Dreher", price: 349, abv: 5}
-let sporoni = {name: "Soproni", price:329, abv: 4.5}
-let borsodi = {name: "Borsodi", price:339, abv: 4.8}
-
-logger(decideIfBeerIsGood(dreher))
-logger(decideIfBeerIsGood(sporoni))
-logger(decideIfBeerIsGood(borsodi))
+/* for (let index = 0; index < array.length; index++) {
+    rootElement.insertAdjacentHTML("beforeend",`<h2> ${array[index]} </h2>`);
+} */
