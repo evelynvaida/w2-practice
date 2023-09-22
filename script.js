1808a85d66a2f61463cdc7d762707a9802d2c872
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-09-22 21:07:09 +0300

    fetch + css (cards)

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ while (counter < 10) {
 /* for (let index = 0; index < array.length; index++) {
     rootElement.insertAdjacentHTML("beforeend",`<h2> ${array[index]} </h2>`);
 } */
+
+fetch('https://restcountries.com/v3.1/all')
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+        for (let index = 0; index < data.length; index++) {
+            let countryName = data[index].name.common;
+            let countryPopulation = data[index].population;
+            let countryArea = data[index].area;
+
+            rootElement.insertAdjacentHTML("beforeend", `
+                <div class="country"> 
+                 <h2> ${countryName} </h2>
+                 <p> population: ${countryPopulation} </p>
+                 <p> area: ${countryArea} km2 </p>
+                </div>
+            `);
+        }
+    });
